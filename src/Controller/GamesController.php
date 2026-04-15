0033e5f71911823d394fe5d74215d1e231ff8f97
aziz <mohamedazizbchir61@gmail.come>
2026-04-15 14:53:08 +0100

    Add quiz submission functionality with scoring and result page

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Score;
 use App\Repository\JeuRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,17 +22,41 @@ class GamesController extends AbstractController
         ]);
     }
 
-    #[Route('/games/{id}', name: 'app_games_show')]
-    public function show($id, JeuRepository $jeuRepository): Response
+    #[Route('/games/{id}/submit', name: 'app_games_submit', methods: ['POST'])]
+    public function submit(Request $request, $id, JeuRepository $jeuRepository, EntityManagerInterface $entityManager): Response
     {
         $jeu = $jeuRepository->find($id);
-
         if (!$jeu) {
             throw $this->createNotFoundException('Jeu not found');
         }
 
-        return $this->render('games/show.html.twig', [
+        $answers = $request->request->all();
+        $score = 0;
+        $total = count($jeu->getQuestions());
+
+        foreach ($jeu->getQuestions() as $question) {
+            $userAnswer = $answers['q' . $question->getId()] ?? null;
+            if ($userAnswer === $question->getBonneReponse()) {
+                $score++;
+            }
+        }
+
+        // Assuming user is logged in
+        $user = $this->getUser();
+        if ($user) {
+            $scoreEntity = new Score();
+            $scoreEntity->setJeu($jeu);
+            $scoreEntity->setUtilisateur($user);
+            $scoreEntity->setPoints($score);
+            $scoreEntity->setDatePartie(new \DateTime());
+
+            $entityManager->persist($scoreEntity);
+            $entityManager->flush();
+        }
+
+        return $this->render('games/result.html.twig', [
             'jeu' => $jeu,
+            'score' => $score,
+            'total' => $total,
         ]);
     }
-}
